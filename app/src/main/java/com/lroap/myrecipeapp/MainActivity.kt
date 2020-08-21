c10926bf847b1e5a86f23be672df14b447303f5b
Laura Roa <laualexandrar@gmail.com>
2020-08-21 11:41:35 -0700

    first commit

---
+++ app/src/main/java/com/lroap/myrecipeapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.lroap.myrecipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Toolbar toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+  }
+
+
+}
