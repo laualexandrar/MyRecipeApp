26c3905cb1f185c76c0a55d5a239a8e1df5d5853
Laura Roa <laualexandrar@gmail.com>
2020-08-24 20:59:34 -0700

    second commit

--- app/src/main/java/com/lroap/myrecipeapp/MainActivity.kt
+++ app/src/main/java/com/lroap/myrecipeapp/MainActivity.kt
@@ -4,16 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val recipesList = arrayListOf("Rice with chicken", "Muffins", "Tacos", "Pasta")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        chooseRecipeBtn.setOnClickListener {
+            val random = Random()
+            val randomRecipe = random.nextInt(recipesList.count())
+            selectedRecipeTxt.text = recipesList[randomRecipe]
+        }
+
+        addNewRecipeBtn.setOnClickListener {
+            val newRecipe = addRecipesTxt.text.toString()
+
+            recipesList.add(newRecipe)
+            addRecipesTxt.text.clear()
+
+            print(recipesList)
+        }
+    }
+}
+
 //        Toolbar toolbar = findViewById(R.id.toolbar)
 //        setSupportActionBar(toolbar)
-  }
-
-
-}
---
+++ app/src/main/java/com/lroap/myrecipeapp/view/ui/SplashscreenActivity.kt
@@ -0,0 +1,42 @@
+package com.lroap.myrecipeapp.view.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.lroap.myrecipeapp.MainActivity
+import com.lroap.myrecipeapp.R
+import kotlinx.android.synthetic.main.activity_splashscreen.*
+
+class SplashscreenActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        ivSplashScreenBook.startAnimation(animation)
+
+
+        val intent = Intent(this, MainActivity::class.java )
+
+        animation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+        })
+    }
+
+}
