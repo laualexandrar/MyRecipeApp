958de7403c509bc476528f174f46edd31ef070ec
Laura Roa <laualexandrar@gmail.com>
2020-09-01 01:14:29 -0700

    Lifecycle

--- app/src/main/java/com/lroap/myrecipeapp/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/lroap/myrecipeapp/view/ui/activities/MainActivity.kt
@@ -2,11 +2,14 @@ package com.lroap.myrecipeapp.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.lroap.myrecipeapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
 
     val recipesList = arrayListOf("Rice with chicken", "Muffins", "Tacos", "Pasta")
 
@@ -28,6 +31,31 @@ class MainActivity : AppCompatActivity() {
 
             print(recipesList)
         }
+    }
+
+    override fun onStart() {
+        Log.d(TAG ,"${javaClass.simpleName} OnStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG ,"${javaClass.simpleName} OnResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG ,"${javaClass.simpleName} OnPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG ,"${javaClass.simpleName} OnStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG ,"${javaClass.simpleName} OnDestroy")
+        super.onDestroy()
     }
 }
 
