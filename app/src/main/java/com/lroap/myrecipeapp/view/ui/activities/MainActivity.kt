db9ecd6e9db7ecd929526e8b3dab351a727c825c
Laura Roa <laualexandrar@gmail.com>
2020-09-18 14:07:01 -0700

    adding fragments

--- app/src/main/java/com/lroap/myrecipeapp/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/lroap/myrecipeapp/view/ui/activities/MainActivity.kt
@@ -22,30 +22,30 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        Log.d(TAG ,"${javaClass.simpleName} OnStart")
-        super.onStart()
-    }
-
-    override fun onResume() {
-        Log.d(TAG ,"${javaClass.simpleName} OnResume")
-        super.onResume()
-    }
-
-    override fun onPause() {
-        Log.d(TAG ,"${javaClass.simpleName} OnPause")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG ,"${javaClass.simpleName} OnStop")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG ,"${javaClass.simpleName} OnDestroy")
-        super.onDestroy()
-    }
+//    override fun onStart() {
+//        Log.d(TAG ,"${javaClass.simpleName} OnStart")
+//        super.onStart()
+//    }
+//
+//    override fun onResume() {
+//        Log.d(TAG ,"${javaClass.simpleName} OnResume")
+//        super.onResume()
+//    }
+//
+//    override fun onPause() {
+//        Log.d(TAG ,"${javaClass.simpleName} OnPause")
+//        super.onPause()
+//    }
+//
+//    override fun onStop() {
+//        Log.d(TAG ,"${javaClass.simpleName} OnStop")
+//        super.onStop()
+//    }
+//
+//    override fun onDestroy() {
+//        Log.d(TAG ,"${javaClass.simpleName} OnDestroy")
+//        super.onDestroy()
+//    }
 }
 
 //        Toolbar toolbar = findViewById(R.id.toolbar)
